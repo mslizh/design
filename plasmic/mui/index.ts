d3b9870a95fe09740c7c7bd86d0c9b18aa1e3e0c
Mikhail Slizh <mihalych_i@icloud.com>
2023-06-01 18:53:19 +0600

    added new components registration

--- plasmic/mui/index.ts
+++ plasmic/mui/index.ts
@@ -4,9 +4,17 @@ import {
    Box,
    Button,
    ButtonGroup,
-   LinearProgress,
-   CircularProgress,
    Divider,
+   Card,
+   CardActionArea,
+   CardActions,
+   CardContent,
+   CardHeader,
+   CardMedia,
+   CircularProgress,
+   Container,
+   Drawer,
+   LinearProgress,
 } from "@mui/material";
 import { registerComponent } from "@plasmicapp/host";
 import { appBarMeta } from "./appBarMeta";
@@ -14,9 +22,17 @@ import { badgeMeta } from "./badgeMeta";
 import { boxMeta } from "./boxMeta";
 import { buttonMeta } from "./buttonMeta";
 import { buttonGroupMeta } from "./buttonGroupMeta";
-import { linearProgressMeta } from "./linearProgressMeta";
-import { circularProgressMeta } from "./circularProgressMeta";
 import { dividerMeta } from "./dividerMeta";
+import { cardMeta } from "./cardMeta";
+import { cardActionAreaMeta } from "./cardActionAreaMeta";
+import { cardActionsMeta } from "./cardActionsMeta";
+import { cardContentMeta } from "./cardContentMeta";
+import { cardHeaderMeta } from "./cardHeaderMeta";
+import { cardMediaMeta } from "./cardMediaMeta";
+import { circularProgressMeta } from "./circularProgressMeta";
+import { containerMeta } from "./containerMeta";
+import { drawerMeta } from "./drawerMeta";
+import { linearProgressMeta } from "./linearProgressMeta";
 
 export function registerMuiComponents() {
    registerComponent(AppBar, appBarMeta);
@@ -25,6 +41,14 @@ export function registerMuiComponents() {
    registerComponent(Button, buttonMeta);
    registerComponent(ButtonGroup, buttonGroupMeta);
    registerComponent(Divider, dividerMeta);
+   registerComponent(CardActionArea, cardActionAreaMeta);
+   registerComponent(CardActions, cardActionsMeta);
+   registerComponent(CardContent, cardContentMeta);
+   registerComponent(CardHeader, cardHeaderMeta);
+   registerComponent(CardMedia, cardMediaMeta);
+   registerComponent(Card, cardMeta);
    registerComponent(CircularProgress, circularProgressMeta);
+   registerComponent(Container, containerMeta);
+   registerComponent(Drawer, drawerMeta);
    registerComponent(LinearProgress, linearProgressMeta);
 }
