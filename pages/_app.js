690ee302290d83c5dc3fe10b735f608ddbf3377d
Mikhail Slizh <mihalych_i@icloud.com>
2023-06-06 14:15:58 +0600

    updated plasmic cli

--- pages/_app.js
+++ pages/_app.js
@@ -9,27 +9,27 @@ import { Toaster } from "sonner";
 import styles from "../components/Toaster/Toaster.module.css";
 
 const events = [
-  {
-    start_date: "2023-04-26 6:00",
-    end_date: "2023-04-26 10:00",
-    text: "test",
-    id: 1,
-  },
+   {
+      start_date: "2023-04-26 6:00",
+      end_date: "2023-04-26 10:00",
+      text: "test",
+      id: 1,
+   },
 ];
 
 function MyApp({ Component, pageProps }) {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-  const theme = getTheme(prefersDarkMode ? "dark" : "light");
+   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+   const theme = getTheme(prefersDarkMode ? "dark" : "light");
 
-  return (
-    <ThemeProvider theme={theme}>
-      <PlasmicRootProvider Head={Head}>
-        <Component {...pageProps} />
-        <Toaster closeButton toastOptions={{className: styles.toast}}/>
-        <CssBaseline />
-      </PlasmicRootProvider>
-    </ThemeProvider>
-  );
+   return (
+      <ThemeProvider theme={theme}>
+         <PlasmicRootProvider Head={Head}>
+            <Component {...pageProps} />
+            <Toaster closeButton toastOptions={{ className: styles.toast }} />
+            <CssBaseline />
+         </PlasmicRootProvider>
+      </ThemeProvider>
+   );
 }
 
 export default MyApp;
