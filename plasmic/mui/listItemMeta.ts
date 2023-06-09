6236ab0c5a7226c7201add921949b39c514b5980
Mikhail Slizh <mihalych_i@icloud.com>
2023-06-09 17:49:43 +0600

    refactored list registration

--- plasmic/mui/listItemMeta.ts
+++ plasmic/mui/listItemMeta.ts
@@ -5,7 +5,19 @@ export const listItemMeta: CodeComponentMeta<ListItemProps> = {
    name: "ListItem",
    props: {
       children: "slot",
+      secondaryAction: {
+         type: "slot",
+         hidePlaceholder: true,
+      },
+      divider: "boolean",
       disablePadding: "boolean",
+      disableGutters: "boolean",
+      dense: "boolean",
+      alignItems: {
+         type: "choice",
+         options: ["center", "flex-start"],
+      },
+      sx: "object",
    },
    importPath: "@mui/material",
 };
