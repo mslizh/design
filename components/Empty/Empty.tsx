4f536c025f3401a1d16c426b180e5755d528f24b
Mikhail Slizh <mihalych_i@icloud.com>
2023-06-16 22:09:13 +0600

    refactored Empty registration

--- components/Empty/Empty.tsx
+++ components/Empty/Empty.tsx
@@ -3,26 +3,33 @@ import Typography from "@mui/material/Typography";
 import { EmptyImage } from "@/components/Empty";
 
 export interface EmptyProps {
-  img?: string;
-  message: string;
+   img?: string;
+   message: string;
+   className: string;
 }
 
 export function Empty(props: EmptyProps) {
-  const { img, message } = props;
+   const { img, message, className } = props;
 
-  return (
-    <Stack alignItems="center" spacing={2} p={4} overflow="hidden">
-      <EmptyImage />
-      <Typography
-        variant="body2"
-        color="text.secondary"
-        width="100%"
-        maxWidth={320}
-        textAlign="center"
-        noWrap
+   return (
+      <Stack
+         alignItems="center"
+         spacing={2}
+         p={4}
+         overflow="hidden"
+         className={className}
       >
-        {message}
-      </Typography>
-    </Stack>
-  );
+         <EmptyImage />
+         <Typography
+            variant="body2"
+            color="text.secondary"
+            width="100%"
+            maxWidth={320}
+            textAlign="center"
+            noWrap
+         >
+            {message}
+         </Typography>
+      </Stack>
+   );
 }
--- plasmic/custom/emptyMeta.ts
+++ plasmic/custom/emptyMeta.ts
@@ -2,10 +2,13 @@ import { CodeComponentMeta } from "@plasmicapp/host";
 import { EmptyProps } from "@/components/Empty";
 
 export const emptyMeta: CodeComponentMeta<EmptyProps> = {
-  name: "Empty",
-  props: {
-    img: "string",
-    message: "string",
-  },
-  importPath: "@/components/Empty",
+   name: "Empty",
+   props: {
+      img: "string",
+      message: {
+         type: "string",
+         defaultValue: "Нет данных",
+      },
+   },
+   importPath: "@/components/Empty",
 };
